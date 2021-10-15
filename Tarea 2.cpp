923760b738d2969794378aba1dd133110036ca27
Maria Castro <92470095+Mariacastro28@users.noreply.github.com>
2021-10-14 21:37:54 -0400

    Add files via upload

---
+++ Tarea 2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+ #include<cmath> 
+ #define pi 3.141592 
+using namespace std;
+
+int main() {
+	
+	float radio,area;
+	  
+	 cout<<" ingrese el Radio: ";  
+	cin>>radio;  
+	
+     area=pi*radio*radio;
+     
+ cout<<"El area es: "<<area<<endl;
+   
+     return 0; 
+}
